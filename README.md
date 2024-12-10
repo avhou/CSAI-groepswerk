4ad103f67d3d649e0d40d70905c828f8b11232e0
Joachim Verschelde <joachim.verschelde@gmail.com>
2024-12-10 21:07:45 +0100

    feat: update readme to install cuda, cudnn, pytorch etc

---
+++ README.md
@@ -0,0 +1,16 @@
+# Capita Selecta Group Assignment 3
+TODO FILL IN README.MD
+## Installation
+
+To set up the environment for this project, you need to install the following dependencies:
+
+### CUDA Toolkit
+
+To install the CUDA toolkit follow the instructions on the [NVIDIA CUDA Toolkit website](https://developer.nvidia.com/cuda-toolkit) to install the appropriate version for your system.
+### cuDNN
+
+To install cuDNN download the cuDNN library follow the instructions on the [NVIDIA cuDNN website](https://developer.nvidia.com/cudnn-downloads)to install the appropriate version for your system.
+
+### PyTorch
+
+To install PyTorch for your cuda version, follow the instructions on the [PyTorch website](https://pytorch.org/get-started/locally/).
--- src/main.py
+++ src/main.py
@@ -1,18 +1,30 @@
 import os
-from typing import List, Optional
-
+from dotenv import load_dotenv
+from typing import List
 from llama_index.llms.huggingface import HuggingFaceLLM
 import csv
+import torch
 
 def read_queries_as_list(file_path: str) -> List[str]:
     with open(file_path, mode='r', newline='') as file:
         reader = csv.reader(file)
-        return [row for row in reader]
+        return [row[0] for row in reader] 
 
-HF_TOKEN: Optional[str] = os.getenv("HUGGING_FACE_TOKEN")
-MODEL_NAME: Optional[str] = os.getenv("MODEL_NAME")
-csv_file_path: Optional[str] = os.getenv("CSV_FILE_PATH")
+load_dotenv()
 
-locally_run = HuggingFaceLLM(model_name=MODEL_NAME, hf_token=HF_TOKEN)
+CSV_FILE_PATH = os.getenv('CSV_FILE_PATH')
+MODEL_NAME = os.getenv('MODEL_NAME')
 
-queries = read_queries_as_list(csv_file_path)
+print(f"CSV File Path: {CSV_FILE_PATH}, Model Name: {MODEL_NAME}")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
+
+locally_run = HuggingFaceLLM(model_name=MODEL_NAME, device_map=device)
+
+queries = read_queries_as_list(CSV_FILE_PATH)
+
+for query in queries:
+    print(f"Query: {query}")
+
+responses = locally_run.complete(queries)
