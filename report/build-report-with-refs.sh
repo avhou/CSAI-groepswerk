355acc716c8878cc7c4564866b60146fad692632
Alexander Van Hecke <alexander@m1mac.local>
2024-12-15 10:55:46 +0100

    report template

---
+++ report/build-report-with-refs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euv
+
+rm *.aux *.bbl *.blg *.brf *.log *.out || true
+rm report.pdf || true
+docker run --rm -v "$PWD":/data texlive/texlive bash -c "cd /data && pdflatex report.tex && bibtex report && pdflatex report.tex && pdflatex report.tex" 
+open report.pdf
