c2748d1032343b51b1ed90f82d0382994cfed08d
Alexander Van Hecke <alexander@m1mac.local>
2025-01-11 10:56:08 +0100

    report introduction

--- report/build-report-with-refs.sh
+++ report/build-report-with-refs.sh
@@ -3,5 +3,5 @@ set -euv
 
 rm *.aux *.bbl *.blg *.brf *.log *.out || true
 rm report.pdf || true
-docker run --rm -v "$PWD":/data texlive/texlive bash -c "cd /data && pdflatex report.tex && bibtex report && pdflatex report.tex && pdflatex report.tex" 
+docker run --rm -v "$PWD":/data texlive/texlive bash -c "cd /data && pdflatex report.tex && biber report && pdflatex report.tex && pdflatex report.tex"
 open report.pdf
